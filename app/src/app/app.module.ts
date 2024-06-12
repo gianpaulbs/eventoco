6edc9f0d1fa22376ee060a64ceca5cfd5e3f1ffa
Gianpaul Bernal <gianpaulbernalsaravia@gmail.com>
2024-06-11 23:49:03 -0500

    refactor: reorganizacion de la estructura en core, shared y feature

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturesModule } from './features/features.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, FeaturesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
