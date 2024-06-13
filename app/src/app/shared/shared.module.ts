a4d963cadbc1e2514e56b863ea63d1ece55add8f
Gianpaul Bernal <gianpaulbernalsaravia@gmail.com>
2024-06-13 01:13:35 -0500

    feat: se añadio componente del boton principal

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToggleButtonComponent } from './components/buttons/toggle-button/toggle-button.component';
+import { MainButtonComponent } from './components/buttons/main-button/main-button.component';
 
 @NgModule({
-  declarations: [ToggleButtonComponent],
+  declarations: [ToggleButtonComponent, MainButtonComponent],
   imports: [CommonModule],
-  exports: [ToggleButtonComponent],
+  exports: [ToggleButtonComponent, MainButtonComponent],
 })
 export class SharedModule {}
