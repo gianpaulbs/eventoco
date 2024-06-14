a57f9bd31d544dba751d59f500591ade632bff18
Gianpaul Bernal <gianpaulbernalsaravia@gmail.com>
2024-06-13 23:14:04 -0500

    feat: se añadieron rutas para el login y el registro de usuario

--- app/src/app/features/features.module.ts
+++ app/src/app/features/features.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, AuthModule],
 })
 export class FeaturesModule {}
